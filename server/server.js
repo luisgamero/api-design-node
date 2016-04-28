9b0476d2a42f411c24b1b68da71d9d44f8015208
Luis Gamero <gamero.luis.enrique@gmail.com>
2016-04-27 22:36:04 -0700

    set up app.param for 'id', updateId middleware, and error handling middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-// TODO: user app.params to find the lion using the id
+// TODO: use app.param to find the lion using the id
 // and then attach the lion to the req object and call next. Then in
 // '/lion/:id' just send back req.lion
 
 // create a middleware function to catch and handle errors, register it
 // as the last middleware on app
-
 
 // create a route middleware for POST /lions that will increment and
 // add an id to the incoming new lion object on req.body
@@ -20,9 +19,11 @@ var id = 0;
 
 var updateId = function(req, res, next) {
   // fill this out. this is the route middleware for the ids
+  req.body.id = '' + id++;
+  next();
 };
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -31,6 +32,8 @@ app.use(bodyParser.json());
 app.param('id', function(req, res, next, id) {
   // fill this out to find the lion based off the id
   // and attach it to req.lion. Rember to call next()
+  req.lion = _.find(lions, {id: id})[0];
+  next();
 });
 
 app.get('/lions', function(req, res){
@@ -39,7 +42,7 @@ app.get('/lions', function(req, res){
 
 app.get('/lions/:id', function(req, res){
   // use req.lion
-  res.json(lion || {});
+  res.json(req.lion || {});
 });
 
 app.post('/lions', updateId, function(req, res) {
@@ -54,7 +57,7 @@ app.post('/lions', updateId, function(req, res) {
 app.put('/lions/:id', function(req, res) {
   var update = req.body;
   if (update.id) {
-    delete update.id
+    delete update.id;
   }
 
   var lion = _.findIndex(lions, {id: req.params.id});
@@ -64,6 +67,11 @@ app.put('/lions/:id', function(req, res) {
     var updatedLion = _.assign(lions[lion], update);
     res.json(updatedLion);
   }
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('something broke!');
 });
 
 app.listen(3000);
